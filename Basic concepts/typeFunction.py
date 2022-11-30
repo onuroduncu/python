b72852be3052284c486b8811abb6334da4ec451d
onuroduncu <oduncu.onur4806@gmail.com>
2022-11-30 15:50:40 +0300

    added type function

---
+++ Basic concepts/typeFunction.py
@@ -0,0 +1,11 @@
+#type function
+
+print(type("Hello")) #<class 'str'>
+print(type(3)) #<class 'int'>
+print(type(3.7)) #<class 'float'>
+print(type(3 + 4j)) #<class 'complex'>
+print(type(111)) #<class 'int'>
+print(type("111")) #<class 'str'>
+print(type(3 / 2)) #<class 'float'>
+print(type(3.4 / 2)) #<class 'float'>
+#print(type(3 + 3j) /2) #TypeError: unsupported operand type(s) for /: 'type' and 'int'
