efdf3e816e1b9f96866b8315d1cd1cef8eab31f5
onuroduncu <oduncu.onur4806@gmail.com>
2022-11-30 16:30:15 +0300

    added new lines

--- Basic concepts/variables.py
+++ Basic concepts/variables.py
@@ -46,3 +46,38 @@ print(div) #1.0638297872340425
 abst = b - c
 print(abst) #0.2999999999999998
 print(total * 2) #19.4
+
+#variable swap
+_a = 5
+_b = 6
+_c = 7
+_d = 8
+
+_a , _b = _c , _d
+
+print(_a) #7
+print(_b) #8
+
+#assign variables each other
+a = b
+print(a) #5
+print(b) #5
+
+#single line assign value to variables
+e = f = g = c
+print(e) #4.7
+print(f) #4.7
+print(g) #4.7
+
+#change the type of variable
+print(type(_c)) #<class 'int'>
+_c = str(_c)
+print(type(_c)) #<class 'str'>
+
+s = "abc"
+t = "123"
+print(type(s)) #<class 'str'>
+print(type(t)) #<class 'str'>
+#s = int(s) #ValueError: invalid literal for int() with base 10: 'abc'
+t = int(t)
+print(type(t)) #<class 'int'>
