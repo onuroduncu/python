6382c93a7f105f9b2e30d3a1cff1c299379b586b
onuroduncu <oduncu.onur4806@gmail.com>
2022-11-30 15:01:39 +0300

    added string.py file

---
+++ Basic concepts/String.py
@@ -0,0 +1,43 @@
+#CHARACTER ARRAYS(CHAR - STRING)
+#--------------------------------------------
+
+#For single line comment
+
+"""
+For
+Multiple
+Lines
+"""
+
+print("Hello!") #Hello
+
+#empty string 
+print("") #
+
+#blank
+print(" ") # 
+
+# sum of two string and blank
+print("Hello" + " " + "World") #Hello World
+
+#Actually string is sum of char arrays.
+print("H" "E" "L" "L" "O") #HELLO
+
+print("C" * 6 + "H" * 2 + "A" "R") #CCCCCCHHAR
+
+#Errors
+
+# print("E" * "E") #TypeError: can't multiply sequence by non-int of type 'str'
+# print("S" * 2.5) #TypeError: can't multiply sequence by non-int of type 'float'
+
+#using the reverse slash "\""
+print("Python is \"good\" beginning.") #Python is "good" beginning.
+
+#new line "\n" it should write without blanck with new line if you don't want black character. 
+print("Data\nScience") #Data
+                       #Science
+
+#four unit blank character with "\t" 
+print("Data\tScience") #Data    Science
+
+
