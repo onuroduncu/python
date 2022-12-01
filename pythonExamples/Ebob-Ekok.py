9d33829df02ba796b050b0bdee73154034748206
onuroduncu <oduncu.onur4806@gmail.com>
2022-12-01 18:04:42 +0300

    added python examples files

---
+++ pythonExamples/Ebob-Ekok.py
@@ -0,0 +1,19 @@
+def ebob(number1,number2):
+    great = number1
+    if(number2 > number1):
+        great = number2
+    while(True):
+        if(great % number1 == 0 and great % number2 ==0 ):
+            return great
+            break
+        great=great +1
+
+def ekok(number1,number2):
+    small = number1
+    if(number2 < number1):
+        small = number2
+    while(True):
+        if(number1 % small == 0 and number2 % small ==0 ):
+            return small
+            break
+        small = small -1
