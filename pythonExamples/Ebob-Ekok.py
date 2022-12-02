61a05566d5b4ecf0b7d177983f11930c6ca921f2
onuroduncu <oduncu.onur4806@gmail.com>
2022-12-02 13:52:34 +0300

    editted the line

--- pythonExamples/Ebob-Ekok.py
+++ pythonExamples/Ebob-Ekok.py
@@ -6,7 +6,7 @@ def ebob(number1,number2):
         if(great % number1 == 0 and great % number2 ==0 ):
             return great
             break
-        great=great +1
+        great += 1
 
 def ekok(number1,number2):
     small = number1
@@ -16,4 +16,4 @@ def ekok(number1,number2):
         if(number1 % small == 0 and number2 % small ==0 ):
             return small
             break
-        small = small -1
+        small -= 1
